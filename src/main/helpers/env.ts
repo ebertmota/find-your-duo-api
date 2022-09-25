44516f50bf10d705c4b3265c5fca781967549538
ebertmota <ebert.mota03@gmail.com>
2022-09-25 12:58:26 -0300

    refactor: remove DATABASE_URL from required env vars

--- src/main/helpers/env.ts
+++ src/main/helpers/env.ts
@@ -29,7 +29,6 @@ export const verifyEnvironmentVariables = (): void => {
     'POSTGRESQL_DATABASE',
     'POSTGRESQL_PORT',
     'POSTGRESQL_USERNAME',
-    'DATABASE_URL',
   ];
 
   checkRequiredVars(requiredVars);
