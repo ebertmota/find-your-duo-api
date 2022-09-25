6ecb6b1f654f68b2a8b2f0a1eb873c0c6fbcfdf8
ebertmota <ebert.mota03@gmail.com>
2022-09-25 13:42:37 -0300

    feat: add GET /ads/:id/discord route

--- src/main/routes/ad.ts
+++ src/main/routes/ad.ts
@@ -1,22 +1,11 @@
 import { Router } from 'express';
 import { adaptExpressRoute as adaptRoute } from '../adapters';
-import { makeListAdsController } from '../factories/application/controllers';
+import {
+  makeListAdsController,
+  makeShowAdDiscordController,
+} from '../factories/application/controllers';
 
 export default (router: Router): void => {
   router.get('/ads', adaptRoute(makeListAdsController()));
-  // router.get('/ads/:id/discord', adaptRoute(makeShow))
-  // router.get('/ads/:id/discord', async (request, response) => {
-  //   const ad = await prisma.ad.findUniqueOrThrow({
-  //     where: {
-  //       id: request.params.id,
-  //     },
-  //     select: {
-  //       discord: true,
-  //     },
-  //   });
-
-  //   return response.json({
-  //     discord: ad.discord,
-  //   });
-  // });
+  router.get('/ads/:id/discord', adaptRoute(makeShowAdDiscordController()));
 };
