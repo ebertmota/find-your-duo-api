3e66e26893fa9e8013d8140a4d7ee7590fd4d0d9
ebertmota <ebert.mota03@gmail.com>
2022-09-25 13:41:41 -0300

    feat: ensure AdaptExpressRoute spread request params to controller

--- src/main/adapters/express-router.ts
+++ src/main/adapters/express-router.ts
@@ -4,7 +4,10 @@ import { RequestHandler } from 'express';
 type Adapter = (controller: Controller) => RequestHandler;
 
 export const adaptExpressRoute: Adapter = controller => async (req, res) => {
-  const { statusCode, data } = await controller.handle({ ...req.body });
+  const { statusCode, data } = await controller.handle({
+    ...req.body,
+    ...req.params,
+  });
   const json = statusCode === 200 ? data : { error: data.message };
   res.status(statusCode).json(json);
 };
