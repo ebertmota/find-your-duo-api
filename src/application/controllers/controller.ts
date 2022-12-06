20c2d2a5ae87570fc39e32718c3982203256d7d4
Ebert Mota <ebert.mota03@gmail.com>
2022-12-06 19:31:44 -0300

    feat: ensure Controller input is valid before calls perform

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,16 +1,32 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { HttpResponse, serverError } from '../helpers';
+import { badRequest, serverError } from '../helpers';
+import { HttpResponse, Validator } from '../protocols';
+import { ValidationComposite } from '../validation';
 
 export abstract class Controller {
   abstract perform(input: any): Promise<HttpResponse>;
 
+  buildValidators(httpRequest: any): Validator[] {
+    return [];
+  }
+
   async handle(input: any): Promise<HttpResponse> {
+    const error = this.validate(input);
+    if (error) {
+      return badRequest(error);
+    }
+
     try {
       return await this.perform(input);
     } catch (err) {
       return serverError(err);
     }
+  }
+
+  private validate(httpRequest: any): Error | undefined {
+    const validators = this.buildValidators(httpRequest);
+    return new ValidationComposite(validators).validate();
   }
 }
