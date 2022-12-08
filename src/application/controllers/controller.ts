69f12e6511cf8ead0aa46a10761a912bd2677657
Ebert Mota <ebert.mota03@gmail.com>
2022-12-08 10:32:20 -0300

    refactor: remove validation from application layer

--- src/application/controllers/controller.ts
+++ src/application/controllers/controller.ts
@@ -1,32 +1,17 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { badRequest, serverError } from '../helpers';
-import { HttpResponse, Validator } from '../protocols';
-import { ValidationComposite } from '../validation';
+import { serverError } from '../helpers';
+import { HttpResponse } from '../protocols';
 
 export abstract class Controller {
   abstract perform(input: any): Promise<HttpResponse>;
 
-  buildValidators(httpRequest: any): Validator[] {
-    return [];
-  }
-
   async handle(input: any): Promise<HttpResponse> {
-    const error = this.validate(input);
-    if (error) {
-      return badRequest(error);
-    }
-
     try {
       return await this.perform(input);
     } catch (err) {
+      console.log(err);
       return serverError(err);
     }
-  }
-
-  private validate(httpRequest: any): Error | undefined {
-    const validators = this.buildValidators(httpRequest);
-    return new ValidationComposite(validators).validate();
   }
 }
