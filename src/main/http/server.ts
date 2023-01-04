87fb549e94a8cd530d133cb4aced3adfc02af6cb
Ebert Mota <ebert.mota03@gmail.com>
2023-01-04 10:00:21 -0300

    feat: call serverSwaggerDocs in server config

--- src/main/http/server.ts
+++ src/main/http/server.ts
@@ -2,11 +2,13 @@
 import './config/module-alias';
 import { PgConnection } from '@/infra/postgres/helpers';
 import { env } from '@/main/helpers';
+import { serveSwaggerDocs } from '@/main/http/config/swagger';
 
 PgConnection.getInstance()
   .connect()
   .then(async () => {
     const { app } = await import('@/main/http/config/app');
+    serveSwaggerDocs();
     app.listen(env.appPort, () =>
       console.log(`Sever running at http://localhost:${env.appPort}`),
     );
